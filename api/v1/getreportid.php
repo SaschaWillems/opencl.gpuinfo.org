4ea603f2d5ed3cf6d207c5bf1da2c2cbb068156c
Sascha Willems <webmaster@saschawillems.de>
2021-05-16 14:51:38 +0200

    Report ID route

--- api/v1/getreportid.php
+++ api/v1/getreportid.php
@@ -20,6 +20,9 @@
  *
  */
 
+include './../../database/database.class.php';	
+include './report.class.php';
+
 $MAX_FILESIZE = 512 * 1024;
 $upload_file_name = $_FILES['data']['name'];
 if ($_FILES['data']['size'] > $MAX_FILESIZE) {
@@ -37,41 +40,74 @@ $path = './';
 $file_name = uniqid('getreportid_', true) . 'json';
 move_uploaded_file($_FILES['data']['tmp_name'], $path . $file_name) or exit('Error: Could not store report!');
 
-$jsonFile = file_get_contents($file_name);
-$report = json_decode($jsonFile, true);
-
 try {
-	header('HTTP/1.1 200 report_new');
-	echo "-1";	
-	/*
-	DB::connect();
-	$stmnt = DB::$connection->prepare("SELECT * from reports where
-			devicename = :devicename and 
-			driverversion = :driverversion and
-			osname = :osname and
-			osversion = :osversion and
-			osarchitecture = :osarchitecture and
-			apiversion = :apiversion and
-			id = :reportid");
+	$source = file_get_contents($file_name);
+	$report = new Report();
+	$report->fromJson($source);
+	
 	$params = [
-		'devicename' => $report['properties']['deviceName'],
-		'driverversion' => $report['properties']['driverVersionText'],
-		'osname' => $report['environment']['name'],
-		'osversion' => $report['environment']['version'],
-		'osarchitecture' => $report['environment']['architecture'],
-		'apiversion' => $report['properties']['apiVersionText'],
-		'reportid' => $reportid
+		':devicename' => $report->getDeviceInfoValue('CL_DEVICE_NAME'),
+		':deviceversion' => $report->getDeviceInfoValue('CL_DEVICE_VERSION'),
+		':driverversion' => $report->getDeviceInfoValue('CL_DRIVER_VERSION'),
+		':osname' => $report->getEnvironmentValue('name'),
+		':osversion' => $report->getEnvironmentValue('version'),
+		':osarchitecture' => $report->getEnvironmentValue('architecture'),
 	];
-	$stmnt->execute($params);
-	$report_match = $stmnt->rowCount() > 0;
-	DB::disconnect();
-	if (!$report_match) {
-		header('HTTP/1.1 400 Devices do not match');
-		exit();
+	
+	// Check if all values required to uniquely identify the report are set
+	foreach ($params as $param) {
+		if ($param === null) {
+			header('HTTP/1.1 500 Could not get required report identifiers');
+			if (file_exists($file_name)) {
+				unlink($file_name);
+			}
+			exit();
+		}
 	}
-	*/
+
+	DB::connect();
+	try {
+		$sql = 
+			"SELECT id from reports where 
+				devicename = :devicename and 
+				deviceversion = :deviceversion and
+				driverversion = :driverversion and
+				osname = :osname and
+				osversion = :osversion and
+				osarchitecture = :osarchitecture";
+		try {
+			$stmnt = DB::$connection->prepare($sql);
+			$stmnt->execute($params);
+			$row = $stmnt->fetch(PDO::FETCH_NUM);
+			if ($stmnt->rowCount() > 0) {
+				$reportid = $row[0];
+				header('HTTP/1.1 200 report_present');
+				echo $reportid;
+			} else {
+				header('HTTP/1.1 200 report_not_present');
+				echo "-1";
+			}	
+		} catch (Exception $e) {
+			header('HTTP/1.1 500 Error while trying to get report information');
+			if (file_exists($file_name)) {
+				unlink($file_name);
+			}		
+			exit();
+		}				
+	} catch(Exception $e) {
+		header('HTTP/1.1 500 Error while trying to get report information');
+		if (file_exists($file_name)) {
+			unlink($file_name);
+		}	
+		exit();
+	}	
+	DB::disconnect();
+} catch (Throwable $e) {
+	header('HTTP/1.1 500 Error while trying to get report information');
 } finally {
-	unlink($path . $file_name);
+	if (file_exists($file_name)) {
+		unlink($file_name);
+	}
 }
 
 ?>
