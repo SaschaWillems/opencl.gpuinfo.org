5a5553f881d26ed52691355be2411a91036d4b50
Sascha Willems <webmaster@saschawillems.de>
2021-09-06 19:01:18 +0200

    Preparing download page for first public bete release

--- download.php
+++ download.php
@@ -30,7 +30,7 @@ PageGenerator::header('Download');
 			<h2>Downloads</h2>
 		</div>
 		<div>
-			The database is populated using the OpenCL Hardware Capability Viewer application, available for multiple platforms. It reads and displays Vulkan related information for a selected implementation, and that data can then be uploaded to the database.
+			The database is populated using the OpenCL Hardware Capability Viewer application, available for multiple platforms. It reads and displays OpenCL related information for a selected implementation, and that data can then be uploaded to the database.
 			The OpenCL Hardware Capability Viewer is a free open source product and the sources are always available from <a href="https://github.com/SaschaWillems/OpenCLCapsViewer">this github repository</a>.<br>
 		</div>
 		<div class="page-header">
@@ -38,27 +38,29 @@ PageGenerator::header('Download');
 			<ul>		
 				<li>Windows
 					<ul>
-						<li><a href="downloads/openclcapsviewer_1.00_beta_win64.zip">Windows 64-bit (zip)</a></li>
-						<!-- <li><a href="downloads/vulkancapsviewer_3.01_x86.zip">Windows 32-bit (zip)</a><br/><b>Please note:</b> The 32-bit windows release should only be run on platforms that don't support 64-bit!<br/>Some Vulkan implementations may not expose all hardware capabilities when run under 32 bits.</li> -->
+						<li><a href="downloads/openclcapsviewer_1.00_beta_win64.zip">Windows 64-bit</a> (zip)</li>
 					</ul>
 				</li>
 				<li>Linux
 					<ul>
-						<li><a href="downloads/openclcapsviewer_1.00_beta_linux64.AppImage">X11 x86-64</a> (AppImage)</li>
-						<!-- <li><a href="downloads/vulkancapsviewer_3.01_linux64_wayland.AppImage">Wayland x86-64</a> (AppImage)</li> -->
+						<li><a href="downloads/openclcapsviewer_1.00_linux64_x11.AppImage">X11 x86-64</a> (AppImage)</li>
 					</ul>
 				</li>
 				<li>Android
 					<ul>
-						<li><a href="downloads/openclcapsviewer_1.00_beta_arm.apk">Android arm-v8 (apk)</a></li>
+						<li><a href="downloads/openclcapsviewer_1.00_arm.apk">Android arm-v8</a> (apk)</li>
 					</ul>
 			</ul>
 		</div>
 		<div class="page-header">
 			<h3>Release notes</h3>
-			<h4>1.00 - 2021-xx-yy</h4>
+			<h4>1.00 - 2021-09-12</h4>
 			<ul>
 				<li>First release</li>
+				<ul>
+					<li><b>Note:</b> This is the first public beta release</li>
+					<li>Support for OpenCL 1.x, 2.x and 3.x</li>
+				</ul>
 			</ul>
 		</div>
 	</div>
